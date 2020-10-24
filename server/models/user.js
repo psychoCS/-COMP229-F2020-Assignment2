3104376501ab7185685bdb0c6fe4573316480aeb
psychoCS <thiagoluizb@hotmail.com>
2020-10-24 14:04:39 -0400

    Setting the stage

---
+++ server/models/user.js
@@ -0,0 +1,29 @@
+let mongoose = require('mongoose');
+let passportLocalMongoose = require('passport-local-mongoose');
+
+let Schema = mongoose.Schema;
+let Model = mongoose.model;
+
+let UserSchema = Schema({
+    userName: String,
+    //password: String
+    emails: String,
+    displayName: String,
+    created: 
+    {
+        type: Date,
+        default:Date.now()
+    },
+    updated:
+    {
+        type: Date,
+        default:Date.now()
+    }
+},
+    { 
+        collection: 'users'
+    });
+
+    UserSchema.plugin(passportLocalMongoose);
+
+module.exports.Model = Model('Business', BusinessSchema);
