a40fe057ebf8bdba4357e3b5df266bd9bb3d0377
psychoCS <thiagoluizb@hotmail.com>
2020-10-24 20:11:24 -0400

    created the mongo data base

--- server/config/DB.js
+++ server/config/DB.js
@@ -1,1 +1,2 @@
-module.exports.URL = "mongodb://localhost:27017/business_contacts";
+//module.exports.URL = "mongodb://localhost:27017/business_contacts";
+module.exports.URL = "mongodb+srv://TLB2310:tlb2310@cluster0.ldslt.mongodb.net/TLB?retryWrites=true&w=majority";
